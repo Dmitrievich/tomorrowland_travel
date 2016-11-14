f6c52eb1192496cd661af558e822e3d361bc07db
Dmitrievich <mail_net@ukr.net>
2016-11-14 09:14:25 +0200

    Middleware update.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var express = require('express');
-var fortune = require('./lib/fortune.js');
+var express = require('express'),
+	fortune = require('./lib/fortune.js'),
+	formidable = require('formidable');
 
 var app = express();
+
+var credentials = require('./credentials.js');
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars').create({
@@ -19,11 +22,27 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret,
+}));
 app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser')());
+
+// flash message middleware
+app.use(function(req, res, next){
+	// if there's a flash message, transfer
+	// it to the context, then clear it
+	res.locals.flash = req.session.flash;
+	delete req.session.flash;
+	next();
+});
 
 // set 'showTests' context property if the querystring contains test=1
-app.use(function(req, res, next) {
-	res.locals.showTests = app.get('env') !== 'production' &&
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && 
 		req.query.test === '1';
 	next();
 });
@@ -67,11 +86,11 @@ app.use(function(req, res, next){
 app.get('/', function(req, res) {
 	res.render('home');
 });
-app.get('/about', function(req, res) {
-	res.render('about', {
+app.get('/about', function(req, res){
+	res.render('about', { 
 		fortune: fortune.getFortune(),
-		pageTestScript: '/qa/tests-about.js'
-	});
+		pageTestScript: '/qa/tests-about.js' 
+	} );
 });
 app.get('/tours/hood-river', function(req, res){
 	res.render('tours/hood-river');
@@ -96,9 +115,74 @@ app.get('/data/nursery-rhyme', function(req, res){
 		noun: 'heck',
 	});
 });
+app.get('/thank-you', function(req, res){
+	res.render('thank-you');
+});
+app.get('/newsletter', function(req, res){
+	res.render('newsletter');
+});
+
+// for now, we're mocking NewsletterSignup:
+function NewsletterSignup(){
+}
+NewsletterSignup.prototype.save = function(cb){
+	cb();
+};
+
+var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+
+app.post('/newsletter', function(req, res){
+	var name = req.body.name || '', email = req.body.email || '';
+	// input validation
+	if(!email.match(VALID_EMAIL_REGEX)) {
+		if(req.xhr) return res.json({ error: 'Invalid name email address.' });
+		req.session.flash = {
+			type: 'danger',
+			intro: 'Validation error!',
+			message: 'The email address you entered was  not valid.',
+		};
+		return res.redirect(303, '/newsletter/archive');
+	}
+	new NewsletterSignup({ name: name, email: email }).save(function(err){
+		if(err) {
+			if(req.xhr) return res.json({ error: 'Database error.' });
+			req.session.flash = {
+				type: 'danger',
+				intro: 'Database error!',
+				message: 'There was a database error; please try again later.',
+			};
+			return res.redirect(303, '/newsletter/archive');
+		}
+		if(req.xhr) return res.json({ success: true });
+		req.session.flash = {
+			type: 'success',
+			intro: 'Thank you!',
+			message: 'You have now been signed up for the newsletter.',
+		};
+		return res.redirect(303, '/newsletter/archive');
+	});
+});
+app.get('/newsletter/archive', function(req, res){
+	res.render('newsletter/archive');
+});
+app.get('/contest/vacation-photo', function(req, res){
+    var now = new Date();
+    res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
+});
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+        if(err) return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
+});
 
 // 404 catch-all handler (middleware)
-app.use(function(req, res, next) {
+app.use(function(req, res, next){
 	res.status(404);
 	res.render('404');
 });
@@ -111,6 +195,6 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(app.get('port'), function() {
-	console.log('Express started on "http://localhost:' +
+	console.log('Express started on "http://localhost:' + 
 		app.get('port') + '"; \nPress Ctrl-C to terminate.');
 });
